747b8a1dc8b3b39384b6bcac21ac06c7db19056e
Finlay Morgan <Morgan-F5@ulster.ac.uk>
2025-09-29 15:47:29 +0100

    Added Code

--- CalculatorWeek2/Program.cs
+++ CalculatorWeek2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿CalculatorApp();
+
+void CalculatorApp()
+{
+    int FirstNumber = 0;
+    int SecondNumber = 0;
+    int Result = 0;
+   
+
+    Console.WriteLine("Type in the First Number");
+    FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Type in the Second Number");
+    SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Result = FirstNumber + SecondNumber;
+
+    Result = FirstNumber + SecondNumber;
+    Console.WriteLine("Adding {0} and {1} equals {2}", FirstNumber, SecondNumber, Result);
+}
